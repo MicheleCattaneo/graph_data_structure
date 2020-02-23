53fd43c33d64b124d06b34d9f2410e14b7ddabae
Michele Cattaneo <michelecattaneo18@gmail.com>
2020-02-23 18:09:54 +0100

    update

--- Graph.py
+++ Graph.py
@@ -6,6 +6,9 @@ class Node:
         self.number = number
 
 class Graph:
+    #Constructor:
+    #Given an array of tuples of chars ( where a char represents a Node )
+    #builds the graph
     def __init__(self, edges):
         #A list of tuples of chars
         self.edges = edges
@@ -18,11 +21,14 @@ class Graph:
         #A set of Nodes(name, number)
         self.mapped_nodes = self.mapNode()
         #An array of sets, mapping each node number to the set of its neighborhood
-        self.neighbors = self.makeNeighbors(edges)
+        self.neighbors = self.makeNeighbors()
 
+    # Array<tuple<char>> -> Int
+    # returns the number of edges for the given input
     def countEdges(self, edges):
         return len(edges)
 
+    #Array<tuple<char>> -> Int
     #Return the number of unique nodes given a list of tuples
     #representing the edges of the graph
     #additionally for each new node found, keep track of that number and return it
@@ -37,7 +43,8 @@ class Graph:
                 number += 1
         return number
 
-    #Returns a set of Nodes mapping each 'char' of a node to a number
+    # Nothing -> Set<Node>
+    # Returns a set of Nodes(name, number)
     def mapNode(self):
         number = 0
         setMap = set()
@@ -46,14 +53,40 @@ class Graph:
             number += 1
         return setMap
 
+    # Char -> Optional<Int>
+    # Returns the number onto which the given char is mapped to
+    # or an error if node is not present
+    def getNumber(self, node):
+        filtered = [n for n in self.mapped_nodes if n.name == node]
+        if len(filtered) == 0:
+            raise Exception('No Node found for: {}'.format(node))
+        return filtered[0].number
 
-    def makeNeighbors(self, edges):
+    # Nothing -> Array<Set<Char>>
+    # returns an array A, where A[i] is a set of all neighbors of the node
+    # mapped onto i
+    def makeNeighbors(self):
         neighbors = [0]*self.nodes_number
-        i = 0
 
+        for i in range(self.nodes_number):
+            neighbors[i] = set()
+
+        for e in self.edges:
+            node_index = self.getNumber(e[0])
+            neighbors[node_index].add(e[1])
 
         return neighbors
 
+    # Char -> Set<Char>
+    # Given a char representing a node, returns the set of its neighborhood
+    def getNeighbors(self, node):
+        return self.neighbors[self.getNumber(node)]
 
+# Testing area
 graph = Graph([('A', 'B'), ('B', 'C'), ('B', 'D'), ('C', 'D'), ('E', 'F'), ('F', 'C')])
-print(len(graph.neighbors))
+print(graph.getNeighbors('A'))
+print(graph.getNeighbors('B'))
+print(graph.getNeighbors('C'))
+print(graph.getNeighbors('D'))
+print(graph.getNeighbors('E'))
+print(graph.getNeighbors('F'))
